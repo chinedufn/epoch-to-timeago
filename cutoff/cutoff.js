74b356e54aad1ea7cf13e9bae9285f1e2f2d98e7
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2015-08-19 19:42:43 -0400

    JSON dictionaries

--- cutoff/cutoff.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  seconds: 60,
-  minutes: 60,
-  hours: 24,
-  days: 7,
-  weeks: 4,
-  months: 12
-}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var converter = module.exports = {
-  cutoff: require('./cutoff/cutoff.js'),
-  suffixDictionary: require('./suffix/suffix-dictionary.js'),
+  cutoff: require('./cutoff/cutoff.json'),
+  suffixDictionary: require('./suffix/suffix-dictionary.json'),
   timeCalcs: require('./time-calculations')
 }
 converter.timeAgo = require('./time-ago/time-ago.js').bind(converter)
